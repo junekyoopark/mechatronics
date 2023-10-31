29a083475f3c7daafc4e6efb74b49f801090e149
June-Kyoo <june@June-Kyoos-MacBook-Pro.local>
2023-11-01 01:16:00 +0900

    comments added

--- midterm/PID_TEST_NEWER.c
+++ midterm/PID_TEST_NEWER.c
@@ -1,10 +1,11 @@
+//Code by June-Kyoo Park
 #include <stdio.h>
 #include <wiringPi.h>
 #include <softPwm.h>
 #include <string.h>
-#include <math.h>
+#include <math.h> //needed for float absolute (fabs) function
 
-#define LOOPTIME 1		// Sampling Time
+#define LOOPTIME 1		// Sampling Time, reduced from 5 to 1 for optimal performance.
 
 //RPI PIN DEFINTION//
 #define MOTOR1 19   //RPi pins connected to motor driver
@@ -31,9 +32,11 @@ int encoderPosition = 0;
 float redGearPosition = 0;
 float referencePosition = 0;
 float errorPosition = 0;
-float prevErrorPosition = 0; //ADDED IN DOUBLE PID
+float prevErrorPosition = 0; //USED FOR CASCADED PID, WHICH WAS NOT USED IN PRACTICE DUE TO POOR PERFORMANCE
 
-
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 //DEFINE ENCODER VOID FUNCTIONS
 void funcEncoderA()
 {
@@ -50,7 +53,6 @@ void funcEncoderA()
         else encoderPosition++;
     }
 }
-
 void funcEncoderB()
 {
     encA = digitalRead(ENCODERA);
@@ -67,6 +69,7 @@ void funcEncoderB()
     }
 }
 
+//FOR PULSE FROM DAQ MACHINE
 void funcPulsePin()
 {
     pulsePin = digitalRead(PULSEPIN);
@@ -74,15 +77,21 @@ void funcPulsePin()
         toggle++;
     }
 }
-
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
 //NUMBER OF ITERATIONS
 int num;
 
-#define STOP_TIMER 3000 //maximum time for ITAE stop condition
+//maximum time for ITAE stop condition, USED FOR TESTING
+#define STOP_TIMER 3000 
 
 int checkTime = 0; //used to check time when control while loop starts. Used for datalogging as well.
 
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 // //DATALOGGING//
 #define NUM_ROWS 30000
 #define NUM_COLUMNS 2
@@ -97,53 +106,61 @@ void updateDataArray()
     dataIndex++;
 }
 //////////DATA LOGGING END///////
-
-
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
 int main()
 {
+    //////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////
     //make Data log file
     char filename[100];
     FILE* file;
     printf("Enter the file name: ");
     scanf("%s", filename);
     file = fopen(strcat(filename,".csv"), "w+");
-    //
-
-    //미지수 몇 개 --> nums (scanf로 받기)
+    //////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////
     printf("Enter the number of reference positions: ");
     scanf("%d", &num);
-
     //CHECK INPUT FOR ERROR
     if (num <= 0) {
         printf("Invalid number of reference positions.\n");
         return 1;
     }
-    //사이즈가 num 인 refPosArray, itaeArray 생성
+    //Create refPosArray, itaeArray
     int refPosArray[num];
     float itaeArray[num];
 
-    //array를 인덱싱하면서 (for loop), 원하는 refPosition 한 개씩 input 받기 (scanf)
+    //Getting each reference position and putting them into refPosArray
     for (int i = 0; i < num; i++) {
         printf("Enter reference position %d: ", i+1);
         scanf("%d", &refPosArray[i]); //
-    } //원하는 refPosition들이 담긴 array 가 하나 있는 상태
+    } 
+    //////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////
 
-
-    //for pulse input
+    //RPi GPIO initialization
     wiringPiSetupGpio();
+    //For pulse input
     pinMode(PULSEPIN, INPUT);		// Set PINPULSE as input
     wiringPiISR(PULSEPIN, INT_EDGE_RISING, funcPulsePin);
+    //for Encoders
     pinMode(ENCODERA, INPUT);		// Set ENCODERA as input
     pinMode(ENCODERB, INPUT);		// Set ENCODERB as input
-
     softPwmCreate(MOTOR1, 0, 100);		// Create soft Pwm
     softPwmCreate(MOTOR2, 0, 100); 	// Create soft Pwm
-
     wiringPiISR(ENCODERA, INT_EDGE_BOTH, funcEncoderB);
     wiringPiISR(ENCODERB, INT_EDGE_BOTH, funcEncoderA);
-    
-    //wait until first pinPulse toggle
+    //////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////
+
+    //wait until first pinPulse toggle.
     while(1)
     {
         if(toggle != toggle_before){
@@ -151,10 +168,12 @@ int main()
         }
         //printf("PULSE: %d", toggle);
     }
-    toggle_before = toggle;
-
-    //이젠 for loop 선언할거임 (iterating through i of refPosArray)
-    //for loop iterate through i 
+    toggle_before = toggle; //update toggle_before variable
+    //////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////
+    
+    //iterating through i of refPosArray (iterating through reference positions)
     for(int i = 0; i < num; i++){ 
         //DATA LOGGING TIMER START//
         dataTimer = 0;
@@ -162,19 +181,20 @@ int main()
         //Define ref for this iteration
         referencePosition = refPosArray[i];
 
-
         float integral = 0;
         float prevError = 0;
         float itae = 0;
+
         //checking if looptime has past or not
         int checkTimeBefore = millis();
-        //while ITAE 시간
+        //while ITAE time
         int whileStartTime = millis();
-        //while true, break if pinPulse toggled
+        //while true, break if pinPulse toggled. During testing, change while loop condition to millis()-whileStartTime < LOOPTIME
         while(1){
-            checkTime = millis(); //checkTime is used to check time when while loop starts
+            checkTime = millis(); //checkTime is used to check time everytime while loop starts
             if (checkTime - checkTimeBefore > LOOPTIME){
                 redGearPosition = (float)encoderPosition / ENC2REDGEAR;
+
                 //define position error
                 errorPosition = referencePosition - redGearPosition;
                 //outer loop (position) PID
@@ -182,10 +202,17 @@ int main()
                 //Calculate derivative
                 float derivative = (errorPosition - prevError) / (checkTime - checkTimeBefore);
 
+                //////////////////////////////////////////////////////////////
+                /////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////                
                 //PWM value output (PID)
                 float controlOutput = (errorPosition * PGAIN) + (integral * IGAIN) + (derivative * DGAIN);
+                //////////////////////////////////////////////////////////////
+                /////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////
 
-                //calculate ITAE
+                //calculate ITAE, time milliseconds not seconds so that we don't have to divide by 1000 twice
+                //also useful to see minute changes in ITAE during testing
                 itae += ((checkTime-whileStartTime)) * fabs(errorPosition) * (checkTime - checkTimeBefore);
 
                 printf("Reference Position: %f\n", referencePosition);
@@ -193,7 +220,10 @@ int main()
                 printf("Error in Position: %f\n", errorPosition);
                 printf("ITAE: %f\n", itae);
 
-                //이제 구동
+                //////////////////////////////////////////////////////////////
+                /////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////
+                //Actually give PWM output
                 if (errorPosition > 0)
                 {
                     softPwmWrite(MOTOR2, controlOutput);
@@ -204,16 +234,16 @@ int main()
                     softPwmWrite(MOTOR1, -controlOutput); //minus added since we have to go the other way
                     softPwmWrite(MOTOR2, 0);
                 }
+                //////////////////////////////////////////////////////////////
+                /////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////
                 
                 //update prevError for position
                 prevError = errorPosition;
-
                 if (errorPosition == 0)
                 { 
                     integral = 0; // prevents integral term to accumulate. if integral term is too large, system will be unstable
                 }
-
-
                 
                 updateDataArray(); ///DATA LOGGING
                 checkTimeBefore = checkTime;
@@ -237,7 +267,8 @@ int main()
 
     totalITAE = totalITAE/1000000.0f;
 
-    printf("\n\n\n\n\n\n\n\nTotal ITAE: %f", totalITAE);
+    printf("\n\n\n\n\n\n\n\nTotal ITAE: %f", totalITAE); //print total ITAE at the end
+
     fprintf(file, "\n");
     for (int i = 0; i < dataIndex; i++)
     {
